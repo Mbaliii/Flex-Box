0a835fd32ad90e3bd87e8b1ab0e7aec48aa6ce22
Mbali <mbalikhumalo408@gmail.com>
2023-06-02 10:25:40 +0200

    Flex

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div class="flexbox-container">
+      <div class="flexbox-item-1"/>
+      <div class="flexbox-item-2"/>
+      <div class="flexbox-item-3"/>
+      </div>
+
+    <div class="long-container">
+    <div class="long-item-1"/>
+    <div class="long-item-2"/>
+    <div class="long-item-3"/>
+     </div>
+    
+    
+    <div class="short-container">
+      <div class="short-item short-item-1"/>
     </div>
-  );
+      </>  );
 }
 
 export default App;
